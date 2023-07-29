0a76d47c6c0098b6ddd2c1dafeb829ef205d480b
abolfazldaana <140502862+abolfazldaana@users.noreply.github.com>
2023-07-29 16:40:20 +0330

    bmw

--- cupcakevpn/save_login.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    
-    $data = "Username: " . $username . "\nPassword: " . $password . "\n";
-    file_put_contents("data.txt", $data, FILE_APPEND);
-}
-?>
