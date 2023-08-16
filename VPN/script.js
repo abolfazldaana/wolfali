578e20f16cf8d1c7a7e694d3306283e7fd9eb257
abolfazldaana <140502862+abolfazldaana@users.noreply.github.com>
2023-08-16 13:02:22 +0330

    .

--- VPN/script.js
+++ VPN/script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", () => {
     const linkInput = document.getElementById("linkInput");
     const submitButton = document.getElementById("submitButton");
@@ -8,41 +9,45 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (userLink !== "") {
             const generatedLink = "http://139.144.67.6:8000/check/" + encodeURIComponent(userLink);
+
             fetch(generatedLink)
-            document.getElementById('wait')
             .then(res =>{
                 console.log(res);
             })
+            .then (document.getElementById('wait').innerHTML = ('<h3 data-text=""></h3>'))
+            .then (document.getElementById('wait1').innerHTML = ('<h3 data-text=""></h3>'))
+
             fetch(generatedLink)
 .then(response => response.json()) // تبدیل پاسخ به JSON
 .then(data => {
     // نمایش اطلاعات در صفحه
-    const outputDiv = document.getElementById('output');
-    outputDiv.innerHTML = `
-        <p>آپلود: ${data.upload[0]}</p>
-        <p>دانلود: ${data.download[0]}</p>
-        <p>مجموع : ${data.total[0]}</p>
-        <p>روز های باقی مانده: ${data.remaining_days[0]}</p>
-    `;
+    // const outputDiv = document.getElementById('output');
+    // outputDiv.innerHTML = `
+    //     <p>آپلود: ${data.upload[0]}</p>
+    //     <p>دانلود: ${data.download[0]}</p>
+    //     <p>مجموع : ${data.total[0]}</p>
+    //     <p>روز های باقی مانده: ${data.remaining_days[0]}</p>
+    // `;
+    document.getElementById('upl').innerHTML = (data.upload+'G')
+    document.getElementById('dwl').innerHTML = (data.download+'G')
+    document.getElementById('tot').innerHTML = (data.total+'G')
+    document.getElementById('rd').innerHTML = (data.remaining_days)
 })
 .catch(error => {
     console.error('خطا در دریافت اطلاعات:', error);
     const outputDiv = document.getElementById('output');
     outputDiv.innerHTML = 'خطا در دریافت اطلاعات از API.';
-});
-            // window.open(generatedLink, "_blank");
-            
-            
-            // let up = document.getElementById("up")
-            // up.innerHTML = (generatedLink)
-            // document.write(document.getElementById("up").innerHTML = (generatedLink))
-        }
-        
+            });
+          } 
     });
 });
-$('.input').keypress(function (e) {
-    if (e.which == 13) {
-      $('form#sub').submit();
-      return false;   
-    }
-  });
+const submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('click', () => {
+  submitButton.classList.add('clicked');
+  setTimeout(() => {
+    submitButton.classList.remove('clicked');
+  }, 1000); // زمان تاخیر به میلی‌ثانیه (در این مثال 0.6 ثانیه)
+});
+
+
